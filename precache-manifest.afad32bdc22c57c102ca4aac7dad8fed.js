9f8e1962892d9b46b24faa5a0a9d6340dc57ccb6
kibeom <kibeom.key@gmail.com>
2020-08-03 23:22:47 +0900

    Updates

---
+++ precache-manifest.afad32bdc22c57c102ca4aac7dad8fed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff04c61c9361df4be9d3f6bf230a8ff",
+    "url": "//movie_app_2020/index.html"
+  },
+  {
+    "revision": "9d5c14032400ca5b9ad2",
+    "url": "//movie_app_2020/static/css/main.aef1108b.chunk.css"
+  },
+  {
+    "revision": "2145c4d9656f10e175c4",
+    "url": "//movie_app_2020/static/js/2.ab40a5d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//movie_app_2020/static/js/2.ab40a5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5c14032400ca5b9ad2",
+    "url": "//movie_app_2020/static/js/main.c44a5c68.chunk.js"
+  },
+  {
+    "revision": "b17a9260a65d586f21aa",
+    "url": "//movie_app_2020/static/js/runtime-main.a90dfb4a.js"
+  }
+]);
